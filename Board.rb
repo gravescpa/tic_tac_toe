4905da6fd39632a7b60d2f31a9c20e8177d414ee
Susan Graves <susan@susangravescpa.com>
2016-10-18 16:28:22 -0400

    adding a player def

--- Board.rb
+++ Board.rb
@@ -1,6 +1,6 @@
 # This displays a very simple graphic tic tac toe board
 
-def play_instruction
+def game_instruction
     print "\n"
     puts " 1 | 2 | 3    The positions to play on the tic-tac-toe board are"
     puts "---|---|---   represented by number 1-9 as shown."
@@ -9,4 +9,30 @@ def play_instruction
     puts " 7 | 8 | 9 "
     print "\n"
 end
-play_instruction
+game_instruction
+
+def player1
+    puts "Player 1, would you like to be X or O? "
+    player1 = gets.chomp
+    puts "Great! Player 1 is #{player1}"
+end
+player1
+
+
+# class Board
+#     attr_reader :board
+#     def initialize
+#         @board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+#     end
+
+#     # def printb
+#     #     @board.each do |row|
+#     #         print "|"
+#     #         row.each {|cell| print "#{cell}|"}
+#     #         puts "\n-------"
+#     #     end
+#     # end
+# end
+
+# board = Board.new
+# board.printb
