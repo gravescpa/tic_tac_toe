7c2acf742a49607798a5be4d1d9ef2e970e69298
Susan Graves <susan@susangravescpa.com>
2016-10-18 15:46:53 -0400

    Added instructions

--- Board.rb
+++ Board.rb
@@ -1,10 +1,12 @@
 # This displays a very simple graphic tic tac toe board
 
-def board
+def play_instruction
     print "\n"
-    puts "___|___|___"
-    puts "___|___|___"
-    puts "   |   |"
+    puts " 1 | 2 | 3    The positions to play on the tic-tac-toe board are"
+    puts "---|---|---   represented by number 1-9 as shown."
+    puts " 4 | 5 | 6    To place an 'X' or 'O' in a certain position,"
+    puts "---|---|---   simply enter the number of that position."
+    puts " 7 | 8 | 9 "
     print "\n"
 end
-board
+play_instruction
