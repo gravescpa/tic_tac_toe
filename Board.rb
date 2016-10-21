51ec171bf2e5ba379a7dafd9699179e93edcc6b9
Susan Graves <susan@susangravescpa.com>
2016-10-21 16:17:58 -0400

    haven't a clue

--- Board.rb
+++ Board.rb
@@ -1,32 +1,58 @@
 # This displays a very simple graphic tic tac toe board
+class Ttt
+    # attr_reader :board, :player1, :player2, :pos
 
-def game_instruction
-    print "\n"
-    puts " 1 | 2 | 3    The positions to play on the tic-tac-toe board are"
-    puts "---|---|---   represented by number 1-9 as shown."
-    puts " 4 | 5 | 6    To place an 'X' or 'O' in a certain position,"
-    puts "---|---|---   simply enter the number of that position."
-    puts " 7 | 8 | 9 "
-    print "\n"
-end
-game_instruction
-
-def players
-    puts "Player 1, would you like to be X or O? "
-    player1 = gets.chomp
-    puts "Great! Player 1 is #{player1}"
-  
-    if player1 == "X"
-        player2 = "O"
-    else
-        player2 = "X"
+    def initialize
+        @board = Array.new(3) {Array.new(3, " ")}
     end
-    puts "Great! Player 1 is #{player1} and Player 2 is #{player2}"
-end
-players
 
-def spaces
-    positions = [" "," "," "," "," "," "," "," "," "]
-end
+    def game_instruction
+        print "\n"
+        puts " 1 | 2 | 3    The positions to play on the tic-tac-toe board are"
+        puts "---|---|---   represented by number 1-9 as shown."
+        puts " 4 | 5 | 6    To place an 'X' or 'O' in a certain position,"
+        puts "---|---|---   simply enter the number of that position."
+        puts " 7 | 8 | 9 "
+        print "\n"
+    end
 
+    def players
+        puts "Player 1, would you like to be X or O? "
+        player1 = gets.chomp
+        puts "Great! Player 1 is #{player1}"
+    
+        if player1 == "X"
+            player2 = "O"
+        else
+            player2 = "X"
+        end
+        puts "Great! Player 1 is #{player1} and Player 2 is #{player2}"
+    end
+
+
+    def board
+        (o..2).each do |row|
+            print "       "
+            (0..2).each do |col|
+                print @board[row][col]
+                print " | " unless col == 2
+            end
+            print "\n"
+            print "       ---------\n" unless row == 2
+        end
+        print "\n"
+    end
+       
+
+    def player_move
+        print "\n"
+        print "Player 1, which space do you choose?"
+            move = gets.chomp.to_i - 1
+            row = move / 3
+            col = move % 3
+            return row.col
+    end
+
+end
+ttt()
 
