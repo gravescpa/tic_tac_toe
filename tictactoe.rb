5f509962b87e6c99a02e5ea8754f2a4f0622d214
Susan Graves <susan@susangravescpa.com>
2016-10-22 22:41:26 -0400

    defines board and players X and O

--- tictactoe.rb
+++ tictactoe.rb
@@ -12,7 +12,7 @@ board_fields = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 =>
 
     def game_instruction
         puts ""
-        puts "Let's' play Tic-Tac-Toe?"
+        puts "Let's play Tic-Tac-Toe!"
         print "\n" * 2
         puts " 1 | 2 | 3    The positions to play on the tic-tac-toe board are"
         puts "---|---|---   represented by number 1-9 as shown."
@@ -21,3 +21,24 @@ board_fields = {1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 =>
         puts " 7 | 8 | 9 "
         puts ""
     end
+
+# This section defines the players and their markers
+
+    def players
+        puts "Player 1, would you like to be X or O? "
+        player1 = gets.chomp
+    
+            if player1 == "X"
+                player2 = "O"
+            else
+                player2 = "X"
+            end
+
+        puts "Great! Player 1 is #{player1} and Player 2 is #{player2}"
+        puts ""
+        puts "Let's get started!'"
+    end
+
+
+game_instruction
+players
