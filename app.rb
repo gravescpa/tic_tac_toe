8550ae18e7d0bbb1860ad127ef9574f88131da82
Susan Graves <susan@susangravescpa.com>
2016-11-15 14:52:50 -0500

    asks for and displays name

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'rubygems'
 require_relative 'board.rb'
-# require_relative 'console_game.rb'
 require_relative 'console_human.rb'
 require_relative 'console_random_ai.rb'
 require_relative 'console_sequential_ai.rb'
@@ -16,9 +15,6 @@ get '/' do
     erb :home
 end
 
-post '/home' do
-    name = params[:input_name]
-end
 
 get '/rules' do
     erb :rules
@@ -28,6 +24,25 @@ get '/history' do
     erb :history
 end
 
+get '/player_1_name' do
+    erb :player_1_name
+end
+
+post '/player_1_name' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/player_2_name'
+end
+
+get '/player_2_name' do
+    erb :player_2_name
+end
+
+post '/player_2_name' do
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play_game'
+end
+
 get '/play_game' do
-    erb :play_game, :locals => {:board => session[:board].grid}
+    erb :play_game, :locals => {:board => session[:board].grid, :player_1_name => session[:player_1_name], :player_2_name => session[:player_2_name]}
+    #the above saves your board and lets you pull it everytime you call it?  Pushes your board into the erb.  This is why mob helps i would have spent hours on that
 end
