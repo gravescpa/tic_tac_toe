40a3bde3b182b3ce2672643a2dc13020cd674cf4
Susan Graves <susan@susangravescpa.com>
2016-11-18 18:26:53 -0500

    taking a break

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'rubygems'
-require_relative 'board.rb'
-require_relative 'console_human.rb'
+require_relative 'new_board.rb'
+require_relative 'human.rb'
 require_relative 'console_random_ai.rb'
 require_relative 'console_sequential_ai.rb'
 require_relative 'unbeatable_ai.rb'
@@ -12,8 +12,8 @@ ai = ""
 
 get '/' do
     # @title = "Susan Magic Tic Tac Toe"
-    session[:board] = Board.new
-    erb :home, :locals => { :board => session[:board].grid }
+    session[:board] = Board.new(["","","","","","","","",""])
+    erb :home, :layout => :home_layout, :locals => { :board => session[:board].grid }
 end
 
 get '/rules' do
@@ -21,17 +21,17 @@ get '/rules' do
 end
 
 get '/player_1_name' do
-    erb :player_1_name, :locals => { :board => session[:board].grid }
+    erb :player_1_name, :layout => :home_layout, :locals => { :board => session[:board].grid }
 end
 
 post '/player_1_name' do
     session[:player_1_name] = params[:player_1_name]
-    erb :opponent, :locals => { :board => session[:board].grid }
+    erb :opponent, :layout => :home_layout, :locals => { :board => session[:board].grid }
     # redirect '/choose_opponent'
 end
 
 get '/player_2_name' do
-    erb :player_2_name
+    erb :player_2_name, :layout => :home_layout, :locals => { :board => session[:board].grid }
 end
 
 post '/player_2_name' do
@@ -45,7 +45,8 @@ post '/choose_opponent' do
 	if player_2 == "human"
 		session[:player_2] = Console_human.new("O")
 
-		erb :player_2_name, :locals => { :board => session[:board].grid }
+		erb :player_2_name, :layout => :home_layout, :locals => { :board => session[:board].grid }
+        redirect 'play_game'
 
 	elsif player_2 == "sequential_ai"
 		session[:player_2] = SequentialAI.new("O")
@@ -68,19 +69,21 @@ post '/choose_opponent' do
 end
 
 get '/play_game' do
-    # session[:player_1_name] = params[:player_1]
+    session[:player_1_name] = params[:player_1]
     session[:player_1] = Console_human.new("X")
     session[:current_player] = session[:player_1]
     session[:current_player_name] = session[:player_1_name]
-    erb :play_game, :locals => {:board => session[:board].grid, :player_1_name => session[:player_1_name], :player_2_name => session[:player_2_name]}
+    erb :play_game, :layout => :home_layout, :locals => { :board => session[:board].grid, :current_player_name => session[:current_player_name], :player_1_name => session[:player_1_name], :player_2_name => session[:player_2_name]}
     #the above saves your board and lets you pull it everytime you call it?  Pushes your board into the erb.  
+
+    redirect '/get_move'
 end
 
 get '/get_move' do
-	move = session[:current_player]#.get_move(session[:board].grid)
+	move = session[:current_player].get_move(session[:board].grid)
     
 	if move == "NO"
-        erb :get_move, :locals => { :current_player => session[:current_player], :current_player_name => session[:current_player_name], :board => session[:board].grid }
+        erb :get_move, :layout => :home_layout, :locals => { :player_1_name => session[:player_1_name], :player_2_name => session[:player_2_name], :current_player => session[:current_player], :current_player_name => session[:current_player_name], :board => session[:board].grid }
         
     elsif session[:board].valid_input?(move)
             redirect '/make_move?move=' + move.to_s 
